fe85eb4aff041b9c8a8982a3584ef0f69c5e411f
zwalo <sehoon9867@gmail.com>
2024-05-25 23:20:33 +0900

    Feat: fibonacci

---
+++ cote/lv2/04_fibonacci/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*
+문제 설명
+피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수 입니다.
+
+예를들어
+
+F(2) = F(0) + F(1) = 0 + 1 = 1
+F(3) = F(1) + F(2) = 1 + 1 = 2
+F(4) = F(2) + F(3) = 1 + 2 = 3
+F(5) = F(3) + F(4) = 2 + 3 = 5
+와 같이 이어집니다.
+
+2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
+
+제한 사항
+n은 2 이상 100,000 이하인 자연수입니다.
+*/
+
+func solution(n int) int {
+	fibos := make([]int, 0)
+	fibos = append(fibos, 0)
+	fibos = append(fibos, 1)
+
+	if n >= 2 {
+		for i := 2; i <= n; i++ {
+			fibos = append(fibos, (fibos[i-1]+fibos[i-2])%1234567)
+		}
+	}
+	return fibos[n]
+}
+
+func main() {
+	fmt.Println(solution(3)) // 2
+	fmt.Println(solution(5)) // 5
+}
